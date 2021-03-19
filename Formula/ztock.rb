37e0addecb389430cf9305070606c256987c852f
gaius-qi <gaius.qi@gmail.com>
2021-03-19 18:12:39 +0800

    feat: remove comment

--- Formula/ztock.rb
+++ Formula/ztock.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ztock < Formula
   desc "Command-line for displaying real-time stock data."
   homepage "https://github.com/ztock/ztock"
