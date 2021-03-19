b76a439a71cffce88b89f6a0f912414a6ea05cfd
gaius-qi <gaius.qi@gmail.com>
2021-03-19 18:02:22 +0800

    feat: homepage

--- Formula/ztock.rb
+++ Formula/ztock.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ztock < Formula
   desc "Command-line for displaying real-time stock data."
-  homepage ""
+  homepage "https://github.com/ztock/ztock"
   url "https://github.com/ztock/ztock/archive/v1.0.0.tar.gz"
   sha256 "d34566fa5e840c363ac8b1a2058b2213fcecbcef3c84fdfec625a39405d3da4a"
   license "MIT"
